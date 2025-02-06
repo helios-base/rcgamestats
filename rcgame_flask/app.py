c94281c034bad3092aa96a09c4ff9ac0f55bdc84
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2025-02-06 23:26:56 +0900

    dbdisplay機能を削除し、関連するルーティングとテンプレートを整理

--- rcgame_flask/app.py
+++ rcgame_flask/app.py
@@ -61,9 +61,6 @@ def create_app(test_config=None):
     from rcgame_flask.host import views as host_views
     app.register_blueprint(host_views.host, url_prefix='/host')
 
-    from . import dbdisplay
-    app.register_blueprint(dbdisplay.bp)
-
     from . import communication
     app.register_blueprint(communication.bp)
 

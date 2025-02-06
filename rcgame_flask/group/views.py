34f4f9be15693d0bec936afa8547060f3989498d
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2025-02-06 12:05:47 +0900

    試合情報の参照をIDではなくインデックスを使用するように変更。エンドポイントの書式を整理．

--- rcgame_flask/group/views.py
+++ rcgame_flask/group/views.py
@@ -11,6 +11,11 @@ from rcgame_flask import googlesheet
 group = Blueprint("group", __name__, template_folder="templates", url_prefix="/group")
 
 
+#
+# Group management
+#
+
+
 @group.route("/")
 @login_required
 def index():
@@ -29,6 +34,28 @@ def show_group_matches(group_id):
     """
     matches = Match.query.filter_by(group_id=group_id).all()
     return render_template("group/match_list.html", group_id=group_id, matches=matches)
+
+
+@group.route("/<int:group_id>/logs", methods=["GET"])
+@login_required
+def show_group_logs(group_id):
+    """
+    Show log files for a group.
+    """
+    matches_in_group = Match.query.filter_by(group_id=group_id).all()
+    log_files = []
+    log_directory = None
+
+    logs_dir = os.path.join(current_app.static_folder, "logs")
+    for match in matches_in_group:
+        if match.log_directory_name is not None:
+            log_directory = match.log_directory_name
+            this_log_dir_path = os.path.join(logs_dir, match.log_directory_name)
+            if os.path.exists(this_log_dir_path):
+                #log_files.extend([f for f in os.listdir(this_log_dir_path) if match.log_file_name in f])
+                log_files.extend(glob.glob(os.path.join(this_log_dir_path, f"{match.log_file_name}*")))
+
+    return render_template("group/log_files.html", log_files=log_files, log_directory=log_directory)
 
 
 @group.route("/<int:group_id>/delete", methods=["POST"])
@@ -59,82 +86,6 @@ def delete_group(group_id):
     flash(f"{group_name} has been deleted.")
 
     return redirect(url_for("group.index"))
-
-
-# TODO: POSTメソッドに変更する
-@group.route("/<int:group_id>/reset/<int:match_id>", methods=["POST"])
-@login_required
-def reset_match(group_id, match_id):
-    """
-    Reset a match.
-    """
-    match = Match.query.filter_by(group_id=group_id, match_id=match_id).first()
-    if match and match.processed == "in progress":
-        match.host_name = None
-        match.start_time = None
-        match.processed = "unexecuted"
-        db.session.commit()
-        flash(f"Match {match.match_index} has been reset.")
-    else:
-        flash(f"Match not found or not in progress.")
-
-    return redirect(url_for("group.show_group_matches", group_id=group_id))
-
-
-@group.route("/<int:group_id>/logs", methods=["GET"])
-@login_required
-def show_group_logs(group_id):
-    """
-    Show log files for a group.
-    """
-    matches_in_group = Match.query.filter_by(group_id=group_id).all()
-    log_files = []
-    log_directory = None
-
-    logs_dir = os.path.join(current_app.static_folder, "logs")
-    for match in matches_in_group:
-        if match.log_directory_name is not None:
-            log_directory = match.log_directory_name
-            this_log_dir_path = os.path.join(logs_dir, match.log_directory_name)
-            if os.path.exists(this_log_dir_path):
-                #log_files.extend([f for f in os.listdir(this_log_dir_path) if match.log_file_name in f])
-                log_files.extend(glob.glob(os.path.join(this_log_dir_path, f"{match.log_file_name}*")))
-
-    return render_template("group/log_files.html", log_files=log_files, log_directory=log_directory)
-
-
-@group.route("/<int:group_id>/log/<int:match_id>", methods=["GET"])
-@login_required
-def show_match_log(group_id, match_id):
-    """
-    Show log files for a match.
-    """
-    match = Match.query.get(match_id)
-
-    if match is None:
-        return jsonify({"error": "Match not found"}), 404
-
-    if match.log_directory_name is None:
-        return jsonify({"error": "Log directory not found"}), 404
-    
-    if match.log_file_name is None:
-        return jsonify({"error": "Log file name not found"}), 404
-    
-    log_file_name = match.log_file_name
-    logs_dir = os.path.join(current_app.static_folder, 'logs')
-
-    this_log_dir_path = os.path.join(logs_dir, match.log_directory_name)
-
-    if not os.path.exists(this_log_dir_path):
-        return jsonify({"error": "Log directory not found"}), 404
-
-    #log_files = [f for f in os.listdir(this_log_dir_path) if log_file_name in f]
-    log_files = glob.glob(os.path.join(this_log_dir_path, f"{log_file_name}*"))
-
-    if not log_files:
-        return jsonify({"error": "No matching log files found"}), 404
-
-    return render_template("group/log_files.html", log_files=log_files, log_directory=match.log_directory_name)
 
 
 @group.route("/<int:group_id>/upload_to_google", methods=["POST"])
@@ -170,3 +121,59 @@ def upload_group_results_to_google_sheet(group_id):
         flash("Failed to upload the group results to the Google Spreadsheet.")
 
     return redirect(url_for("group.show_group_matches", group_id=group_id))
+
+#
+# Match management
+#
+
+@group.route("/<int:group_id>/<int:match_index>/reset", methods=["POST"])
+@login_required
+def reset_match(group_id, match_index):
+    """
+    Reset a match.
+    """
+    match = Match.query.filter_by(group_id=group_id, match_id=match_index).first()
+    if match and match.processed == "in progress":
+        match.host_name = None
+        match.start_time = None
+        match.processed = "unexecuted"
+        db.session.commit()
+        flash(f"Match {match.match_index} has been reset.")
+    else:
+        flash(f"Match not found or not in progress.")
+
+    return redirect(url_for("group.show_group_matches", group_id=group_id))
+
+
+@group.route("/<int:group_id>/<int:match_index>/log", methods=["GET"])
+@login_required
+def show_match_log(group_id, match_index):
+    """
+    Show log files for a match.
+    """
+    match = Match.query.filter_by(group_id=group_id, match_id=match_index).first()
+
+    if match is None:
+        return jsonify({"error": "Match not found"}), 404
+
+    if match.log_directory_name is None:
+        return jsonify({"error": "Log directory not found"}), 404
+
+    if match.log_file_name is None:
+        return jsonify({"error": "Log file name not found"}), 404
+
+    log_file_name = match.log_file_name
+    logs_dir = os.path.join(current_app.static_folder, 'logs')
+
+    this_log_dir_path = os.path.join(logs_dir, match.log_directory_name)
+
+    if not os.path.exists(this_log_dir_path):
+        return jsonify({"error": "Log directory not found"}), 404
+
+    #log_files = [f for f in os.listdir(this_log_dir_path) if log_file_name in f]
+    log_files = glob.glob(os.path.join(this_log_dir_path, f"{log_file_name}*"))
+
+    if not log_files:
+        return jsonify({"error": "No matching log files found"}), 404
+
+    return render_template("group/log_files.html", log_files=log_files, log_directory=match.log_directory_name)

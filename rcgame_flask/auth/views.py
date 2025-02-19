5f18118f92af004c923e8cc8efcb849cf9a15569
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2025-02-20 01:51:27 +0900

    ユーザー追加フォームをユーザー一覧画面に統合

--- rcgame_flask/auth/views.py
+++ rcgame_flask/auth/views.py
@@ -206,6 +206,79 @@ def login_google_callback():
 # Admin actions
 #
 
+@auth.route("/admin/users", methods=["GET", "POST"])
+@login_required
+@admin_required
+def show_users():
+    """
+    Show users
+    """
+    form = UserRegistrationForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        try:
+            type = UserType(form.type.data)
+        except ValueError:
+            type = UserType.USER
+
+        if username == "":
+            username = email
+
+        new_user = User(username=username, email=email, type=type)
+        new_user.set_password(password)
+        new_user.auth_provider = "local"
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except Exception as e:
+            flash(f"Error: {e}")
+            return redirect(url_for("auth.register"))
+
+        if not AllowedEmail.query.filter_by(email=email).first():
+            allowed_email = AllowedEmail(email=email, type=type)
+            try:
+                db.session.add(allowed_email)
+                db.session.commit()
+            except Exception as e:
+                flash(f"Error: {e}")
+
+        api_key = APIKey(
+            key=APIKey.generate_api_key(),
+            user_id=new_user.id,
+            scope=type
+        )
+        db.session.add(api_key)
+        db.session.commit()
+
+        flash(f"New user [{username}] registered")
+    
+    users = User.query.all()
+    return render_template("auth/users.html", users=users, form=form)
+
+
+@auth.route("/admin/bulk_delete_users", methods=["POST"])
+@login_required
+@admin_required
+def bulk_delete_users():
+    """
+    Bulk delete users
+    """
+    user_ids = request.form.getlist("user_ids")
+    count = 0
+    for user_id in user_ids:
+        record = User.query.filter_by(id=user_id).first()
+        if record:
+            if record.email == current_user.email:
+                flash("Cannot delete own user")
+                continue
+            count += 1
+            db.session.delete(record)
+    db.session.commit()
+    flash(f"Deleted {count} users")
+    return redirect(url_for("auth.show_users"))
+
 
 @auth.route("/admin/allowed_emails", methods=["GET", "POST"])
 @login_required

daa3e44f9f422a532a3dfae4f3cfd85cf293cb51
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2025-02-20 12:13:31 +0900

    データベース初期化時にユーザーを登録する機能を追加

--- rcgame_flask/create_db.py
+++ rcgame_flask/create_db.py
@@ -2,11 +2,41 @@ import os
 import csv
 import click
 import secrets
-from flask import current_app
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from rcgame_flask.app import db
 from rcgame_flask.auth.models import UserType, User, AllowedEmail, APIKey
 from rcgame_flask.team.models import Team
 from rcgame_flask.config import config
+
+
+def __load_default_users(csv_path):
+    users = []
+    passwords = []
+    with open(csv_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        if set(reader.fieldnames) != set(["username", "email", "type"]):
+            raise ValueError(f"Invalid header in {csv_path}")
+        for line_num, row in enumerate(reader, start=1):
+            username = row.get('username', '').strip()
+            email = row.get('email', '').strip()
+            type = row.get('type', '').strip()
+            if not username or not email:
+                print(f"Invalid user in {csv_path} line {line_num}")
+                continue
+
+            try:
+                user_type = UserType(type)
+            except ValueError:
+                print(f"Invalid user type in {csv_path} line {line_num}")
+                continue
+
+            user = User(username=username, email=email, type=user_type)
+            password = secrets.token_urlsafe(8)
+            user.set_password(password)
+
+            users.append(user)
+            passwords.append(password)
+    return users, passwords
 
 
 def __load_default_gmails(csv_path):
@@ -76,11 +106,45 @@ def init_db():
     admin.email = admin_email
     admin.set_password(initial_password)
     db.session.add(admin)
-    db.session.commit()
+    
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+        print(f'Error adding admin user: {e}')
+        return
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        print(f'Error adding admin user: {e}')
+        return
+
+    #
+    # create default users
+    #
+    print("===== Adding default users =====")
+    try:
+        default_users_csv = os.path.join(base_dir, 'default_users.csv')
+        users, passwords = __load_default_users(default_users_csv)
+        for user, password in zip(users, passwords):
+            print(f"Adding {user.username}, {user.email}, {user.type.value}, {password}")
+            db.session.add(user)
+    except ValueError as e:
+        print(f'Error loading default users: {e}')
+
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+        print(f'Error adding default users: {e}')
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        print(f'Error adding default users: {e}')
 
     #
     # create default allowed emails
     #
+    print("")
+    print("===== Adding default allowed emails =====")
     try:
         default_gmails_csv = os.path.join(base_dir, 'default_allowed_emails.csv')
         allowed_emails = __load_default_gmails(default_gmails_csv)
@@ -89,11 +153,21 @@ def init_db():
             db.session.add(email)
     except ValueError as e:
         print(f'Error loading default allowed emails: {e}')
-    db.session.commit()
+
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+        print(f'Error adding default allowed emails: {e}')
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        print(f'Error adding default allowed emails: {e}')
 
     #
     # register default teams
     #
+    print("")
+    print("===== Adding default teams =====")
     try:
         default_teams_csv = os.path.join(base_dir, 'default_teams.csv')
         teams = __load_default_teams(default_teams_csv)
@@ -102,7 +176,13 @@ def init_db():
             db.session.add(team)
     except ValueError as e:
         print(f'Error loading default teams: {e}')
-    db.session.commit()
+
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        print(f'Error adding default allowed emails: {e}')
+    except SQLAlchemyError as e:
+        print(f'Error adding default allowed emails: {e}')
 
     #
     # create default API key for the admin user
@@ -110,12 +190,31 @@ def init_db():
     default_key = config.ADMIN_API_KEY if config.ADMIN_API_KEY else secrets.token_urlsafe(16)
     default_api_key = APIKey(key=default_key, user_id=admin.id)
     db.session.add(default_api_key)
-    db.session.commit()
+    try:
+        db.session.commit()
+    except IntegrityError as e:
+        print(f'Error adding default API key: {e}')
+    except SQLAlchemyError as e:
+        print(f'Error adding default API key: {e}')
 
+    print("")
+    print("===== Initialization complete =====")
     print(f"Admin Username: {admin.username}")
     print(f"Admin Email: {admin.email}")
     print(f"Admin password: {initial_password}")
     print(f"Admin API key: {admin.api_keys.first().key}")
+    print("")
+    print("Please change the admin password and API key immediately.")
+    print("")
+    print("The default users, emails, and teams have been added.")
+    print("Please review the default users and emails and remove any that are not needed.")
+    print("The default teams have been added. Please review and update the teams as needed.")
+    print("")
+    print("The default users, emails, and teams are defined in the following files:")
+    print(f"  {default_users_csv}")
+    print(f"  {default_gmails_csv}")
+    print(f"  {default_teams_csv}")
+    print("")
 
 
 @click.command("init-db")

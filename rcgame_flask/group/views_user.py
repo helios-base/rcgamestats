7cba0bbaf9e0c7ece3612a6828773ffba78f0562
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2025-02-27 02:19:20 +0900

    statsページの最終更新時間を考慮したデータ更新チェック機能を追加

--- rcgame_flask/group/views_user.py
+++ rcgame_flask/group/views_user.py
@@ -277,17 +277,24 @@ def has_updates():
     """
     Check if there are any updates to the groups.
     """
-    last_updated_at = request.args.get("last_updated_at")
+    last_updated_at = request.args.get("page_load_at")
+    # print(f"page_load_at: {last_updated_at}")
     # If last_updated_at is None, return True as there are updates.
     if last_updated_at is None:
         return jsonify({"update": True})
 
     try:
-        last_updated_at = datetime.fromisoformat(last_updated_at)
+        if last_updated_at.isdigit():
+            timestamp = int(last_updated_at) / 1000.0
+            last_updated_at_dt = datetime.fromtimestamp(timestamp)
+        else:
+            last_updated_at_dt = datetime.strptime(last_updated_at, "%Y-%m-%d %H:%M:%S")
+        # print(f"page_load_at: {last_updated_at_dt}")
     except ValueError:
         return jsonify({"error": "Invalid datetime format"}), 400
 
-    group_list = Group.query.filter(Group.updated_at > last_updated_at).all()
+    group_list = Group.query.filter(Group.updated_at > last_updated_at_dt).all()
     has_updates = len(group_list) > 0
 
-    return jsonify({"has_updates": has_updates})
+    # print(f"has_updates: {has_updates}")
+    return jsonify({"update": has_updates})

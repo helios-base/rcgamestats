2e5b2cd0a56c87c8ab8765ffeba532b42569bbad
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2025-03-21 22:22:45 +0900

    init_clients.shの内部処理をinit_one.shに分離

--- tools/init_clients.sh
+++ tools/init_clients.sh
@@ -8,14 +8,6 @@ cd `dirname $0`
 sort -u $HOST_LIST > tmp_hosts
 
 for host in `cat tmp_hosts`; do
-    echo "=========="
-    echo "Copying client to $host"
-    rsync -auzv ~/local/ $host:local/
-    rsync -auzv ~/.rcssserver/ $host:.rcssserver/
-    rsync -auzv ../client/ $host:${REMOTE_CLIENT_DIR}/
-    echo "==="
-    echo "Initializing virtual environment on $host"
-    ssh $host "${REMOTE_CLIENT_DIR}/init_venv.sh"
-    echo "Done"
+    ./init_one.sh $host
 done
 

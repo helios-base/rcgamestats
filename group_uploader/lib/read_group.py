a6e55c05e73d27d6b8332c29ab626c3082bd33f0
arata55555 <arata528528@gmail.com>
2025-06-02 09:44:20 +0900

    csvファイルからdominate_timeを読み取り，返すように変更

--- group_uploader/lib/read_group.py
+++ group_uploader/lib/read_group.py
@@ -30,6 +30,22 @@ def get_scores(result_file):
 
     return None, None
 
+def get_dominate_time(loganalyzer3_file):
+    our_dominate_time = 0
+    opp_dominate_time = 0
+    with open(loganalyzer3_file, "r") as f:
+        reader = csv.DictReader(f, skipinitialspace=True)
+        for row in reader:
+            try:
+                if row["our dominate time"]:
+                    our_dominate_time = int(row["our dominate time"])
+                if row["opp dominate time"]:
+                    opp_dominate_time = int(row["opp dominate time"])
+            except ValueError:
+                return None, None
+            except KeyError:
+                return None, None
+        return our_dominate_time, opp_dominate_time
 
 def get_results(directory):
     result_files = os.path.join(directory, "*.result.csv")
@@ -42,3 +58,15 @@ def get_results(directory):
         if exist_rcl_and_rcg_file(directory, base):
             results.append((base, left_score, right_score))
     return results
+
+def get_loganalyzer3(directory):
+    loganalyzer3_files = os.path.join(directory, "*.loganalyzer3.csv")
+    loganalyzer3 = []
+    for loganalyzer3_file in glob.glob(loganalyzer3_files):
+        our_dominate_time, opp_dominate_time = get_dominate_time(loganalyzer3_file)
+        if our_dominate_time is None or opp_dominate_time is None:
+            continue
+        base = os.path.basename(loganalyzer3_file)[:-len(".loganalyzer3.csv")]
+        if exist_rcl_and_rcg_file(directory, base):
+            loganalyzer3.append((base, our_dominate_time, opp_dominate_time))
+    return loganalyzer3
